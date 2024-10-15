61a3bcb29cf1678557440f1ecf98634e2fde4a46
Shivam Rakhunde <shivamrakhunde03@gmail.com>
2024-10-15 16:57:29 +0530

    add name

--- src/ConnectionPackage/ConnectionProvider.java
+++ src/ConnectionPackage/ConnectionProvider.java
@@ -3,25 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ConnectionPackage;
+
 import java.sql.*;
+
 /**
  *
- * @author Shantanu
+ * @author shivam
  */
-public class ConnectionProvider 
-{
-    public static Connection getcon()
-    {
-        try
-        {
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/billing","root","11223344");
+public class ConnectionProvider {
+    public static Connection getcon() {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/billing", "root", "11223344");
             return con;
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             return null;
         }
     }
-    
-    
+
 }
